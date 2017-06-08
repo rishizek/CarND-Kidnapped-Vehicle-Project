3f5f02fe7102a96b1ceec74b0d108e8ab17d786a
Riei Ishizeki <rishizek@gmail.com>
2017-06-08 18:28:05 +0900

    Change the number of particles to 100

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -24,7 +24,7 @@ void ParticleFilter::init(double x, double y, double theta, double std[]) {
 	//   x, y, theta and their uncertainties from GPS) and all weights to 1. 
 	// Add random Gaussian noise to each particle.
 	// NOTE: Consult particle_filter.h for more information about this method (and others in this file).
-  num_particles = 1000;
+  num_particles = 100;
   default_random_engine gen;
   
   normal_distribution<double> dist_x(x, std[0]);
